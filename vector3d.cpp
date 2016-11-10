5428cbdd232c1a1e65decf544798e3cafcda3556
lev1917 <lev.ryskin@mail.ru>
2016-11-10 21:34:24 +0300

    Update vector3d.cpp

--- vector3d.cpp
+++ vector3d.cpp
@@ -1,10 +1,23 @@
 #include "vector3d.hpp"
 
 #include <cmath>
-
+#include <iostream>
 const vector3d vector3d::X = {1, 0, 0};
 const vector3d vector3d::Y = {0, 1, 0};
 const vector3d vector3d::Z = {0, 0, 1};
 const vector3d vector3d::ZERO = {0, 0, 0};
-
-// TODO: дописать
+vector3d::vector3d(int _x=0,int _y=0,int _z=0){
+x=_x,y=_y,z=_z;}
+vector3d:: vector3d operator+(const vector3d &other) const {return vector3d(x+other.x,y+other.y+z+other.z);}
+vector3d::vector3d operator-(const vector3d &other) const {return vector3d(x-other.x,y-other.y,z-other.z);}
+vector3d:: vector3d operator^(const vector3d &other) const {return vector3d(y*other.z-z*other.y,z*other.x-x*other.z,x*other.y-y*other.x);}
+vector3d::double operator*(const vector3d &other) const {return (x*other.x+y*other.y+z*other.z);}
+vector3d::vector3d operator*(double _x) const {return vector3d(x*_x,y*_x,z*_x);}
+vector3d::vector3d operator-() const {return (*this)*(-1);}
+vector3d::double length() const{return std::pow(((*this)*(*this)),0.5);}
+vector3d::void normalize(){double len=(*this).length;
+x=x/len;
+y=y/len;
+z=z/len;}
+std::ostream &operator«(std::ostream &stream, const vector3d& a) {
+return stream « "{"«a.x«","«a.y«","«a.z«"}";}
