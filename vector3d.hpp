2088c831d5780b14cb449a41d98c3f79bb817854
lev1917 <lev.ryskin@mail.ru>
2016-11-10 21:13:45 +0300

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -5,15 +5,26 @@
 
 class vector3d {
 public:
+    friend vector3d operator*(double x, const vector3d &v) const;
+    friend std::ostream &operator«(std::ostream &stream, const vector3d& a) const;
+    double x,y,z;
     const static vector3d X;
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
-
-    // TODO: дописать
-
+    vector3d(int _x=0,int _y=0,int _z=0);
+    vector3d operator+(const vector3d &other) const;
+    vector3d operator-(const vector3d &other) const;
+    vector3d operator^(const vector3d &other) const;
+    double operator*(const vector3d &other)const;
+    vector3d operator*(double _x)const;
+    vector3d operator-();
+    double length() const;
+    void normalize();
 };
 
-// TODO: дописать
+
+
+
 
 #endif
